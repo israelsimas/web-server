251aa725d804a8bad2a8cb6b4903338e2a45fba5
Your Name <you@example.com>
2019-06-16 20:46:11 -0300

    exporte de contatos

--- app/src/web_system_request.lua
+++ app/src/web_system_request.lua
@@ -1,13 +1,11 @@
-
-
-local selWAN = "PK,ETHAddressIP,ETHNetMask,ETHDefGateway,ETHPriDNS,ETHSecDNS,ETHLinkBandwidthUpload,ETHLinkBandwidthDownload,ETHActivateDHCPClient,ETHSpeed,ETHTrafficEnableSIP,ETHTrafficEnableRTP,ETHTrafficEnableAdmin, ETHQoSEnableLayer3,ETHQoSTypeForSIP,ETHQoSValueForSIP,ETHQoSTypeForRTP,ETHQoSValueForRTP,ETHQoSTypeForAdmin,ETHQoSValueForAdmin,ETHHostname,ETHDomainName,ETHPPPoE,ETHPPPoEUserName,ETHPPPoEPassword,ETHPPPoEDownTime,ETHPPPoEEcoCount,ETHBroadcastLimit,ETHMulticastLimit,Enable8021X,Protocol8021X,User8021X,Password8021X"
-  
+local xml = require('LuaXML')
+ 
 function exportAutoprovisioning()
 
   require('db_driver')
 
+  local selWAN = "PK,ETHAddressIP,ETHNetMask,ETHDefGateway,ETHPriDNS,ETHSecDNS,ETHLinkBandwidthUpload,ETHLinkBandwidthDownload,ETHActivateDHCPClient,ETHSpeed,ETHTrafficEnableSIP,ETHTrafficEnableRTP,ETHTrafficEnableAdmin, ETHQoSEnableLayer3,ETHQoSTypeForSIP,ETHQoSValueForSIP,ETHQoSTypeForRTP,ETHQoSValueForRTP,ETHQoSTypeForAdmin,ETHQoSValueForAdmin,ETHHostname,ETHDomainName,ETHPPPoE,ETHPPPoEUserName,ETHPPPoEPassword,ETHPPPoEDownTime,ETHPPPoEEcoCount,ETHBroadcastLimit,ETHMulticastLimit,Enable8021X,Protocol8021X,User8021X,Password8021X"
   local qTableName = DbDriver.execute("SELECT name FROM sqlite_master WHERE type = 'table';");
-  local xml = require('LuaXML')
   local autoprovisioningData = xml.new("autoprovisioning")
   local fields = {}
 
@@ -101,10 +99,116 @@ function exportAutoprovisioning()
   file:close()
 end
 
+
+ContactParse = {}
+
+xml_to_contacts_mapping = {sDisplayName="IdentificationName", sOfficeNumber="OfficeNumber", sMobilNumber="MobilePhone", sOtherNumber="OtherNumber", sRing="DistintiveRing", sAccount="Account", sIcon="Icon"}
+
+function ContactParse:parseXMLContact(xmlText,tag)
+  
+  local contacts = {}
+  local xfile = xml.eval(xmlText)
+  local group = xfile:find(tag)
+  
+  for i=1,#group do
+    local contact = {}
+    for k,v in pairs(group[i]) do
+      if xml_to_contacts_mapping[k] ~= nil then
+        contact[xml_to_contacts_mapping[k]] = v
+      end
+    end
+    if ( (contact[xml_to_contacts_mapping["sDisplayName"]] ~= nil) and (contact[xml_to_contacts_mapping["sOfficeNumber"]] ~= nil) and (contact[xml_to_contacts_mapping["sRing"]] ~= nil) and (contact[xml_to_contacts_mapping["sAccount"]] ~= nil) ) then
+      table.insert(contacts,contact)
+    else
+      print('Invalid Contact')
+    end
+  end
+
+  return contacts
+end
+
+function ContactParse:parseContacts(file)
+  return self:parseXMLContact(file,"group")
+end
+
+function ContactParse:parseBlackList(file)
+  return self:parseXMLContact(file,"blacklist")
+end
+
+ContactGenerator = {}
+
+function ContactGenerator:toXML(contacts,contactsBlacklist)
+  local contactData = xml.new("contactData")
+
+  if contacts then
+    local group =  contactData:append("group")
+    
+    for i=1,#contacts do
+      
+      local contact            = group:append("contact")
+      contact["sDisplayName"]  = contacts[i].IdentificationName
+      contact["sOfficeNumber"] = contacts[i].OfficeNumber
+      contact["sRing"]         = contacts[i].DistintiveRing
+      contact["sAccount"]      = contacts[i].Account
+      contact["sIcon"]         = contacts[i].Icon
+    end
+
+  end
+  if contactsBlacklist then
+    local blacklist =  contactData:append("blacklist")
+    
+    for i=1,#contactsBlacklist do
+
+      local contactBlacklist             = blacklist:append("contact")
+      contactBlacklist["sDisplayName"]   = contactsBlacklist[i].IdentificationName
+      contactBlacklist["sOfficeNumber"]  = contactsBlacklist[i].OfficeNumber
+      contactBlacklist["sRing"]          = contactsBlacklist[i].DistintiveRing
+      contact["sAccount"]                = contacts[i].Account
+    end
+  end
+
+  return contactData
+end
+
+function exportContacts(export_ids)
+
+  print("exporting contacts ...")
+  require('db_driver')
+  
+  local ids = export_ids:split(",")
+
+  local where_clauses = nil
+
+  for i=1, #ids do
+    if where_clauses then
+      where_clauses = where_clauses .. " or PK = " .. ids[i]
+    else
+      where_clauses =  " PK = ".. ids[i]
+    end
+
+  end
+
+  local selectSql = "SELECT PK, Account, IndexPhoneBook, IdentificationName, OfficeNumber, DistintiveRing, Icon FROM TAB_PHONEBOOK WHERE " .. where_clauses .. " ORDER BY PK ASC;"
+
+  require('db_driver')
+  local cnx = Db:getConnection()
+  local contacts = DbDriver.executeSelect(selectSql, cnx)
+  cnx:close()
+
+  blacklist = {}
+
+  local contactData = ContactGenerator:toXML(contacts.rows, blacklist)
+
+  local xmlStr = '<?xml version="1.0" encoding="UTF-8"?>\n' .. tostring(contactData);
+  local file = io.open("/data/contacts_exported.xml", "w")
+  file:write(xmlStr)
+  file:close()
+end
+
 if (arg[1] == "export_autoprov") then
   exportAutoprovisioning()
-elseif (arg[1] == "stop") then
-  stop()  
+elseif (arg[1] == "export_contacts") then
+  exportContacts(arg[2])
 elseif (arg[1] == "restart") then
   restart()
 end
